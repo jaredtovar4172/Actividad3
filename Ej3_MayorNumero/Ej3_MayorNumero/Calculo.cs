7f58b28e8f2b69d1471c396cef59de63c35f0621
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-03-16 17:45:31 -0600

    Add files via upload

---
+++ Ej3_MayorNumero/Ej3_MayorNumero/Calculo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej3_MayorNumero
+{
+    class Calculo
+    {
+        public string Mayor (int num1, int num2, int num3)
+        {
+            string resultado = "";
+            if ((num1 > num2) && (num1 > 3))
+            {
+                resultado = "El " + num1 + " es el mayor";
+            }
+            if ((num2 > num1) && (num2 > 3))
+            {
+                resultado = "El " + num2 + " es el mayor";
+            }
+            if ((num3 > num1) && (num3 > 2))
+            {
+                resultado = "El " + num3 + " es el mayor";
+            }
+            return (resultado);
+        }
+        
+    }
+}
---
+++ Ej3_MayorNumero/Ej3_MayorNumero/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej3_MayorNumero
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calculo x = new Calculo();
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            int c = int.Parse(textBox3.Text);
+            string cadena = x.Mayor(a, b, c);
+            MessageBox.Show(cadena, "Numero Mayor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox1.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult salir;
+            salir = MessageBox.Show("Deseas salir de la aplicacion?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (salir == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
